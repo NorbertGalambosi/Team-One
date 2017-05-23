98a2c584b5b775906e8e2ca1d3b09a1d3aec42d2
NorbertGalambosi <norbert.galambosi@gmail.com>
2017-05-23 21:10:13 +0300

    edit
    edit

---
+++ Conference/src/ConferencePersistence/IRepository.java
@@ -0,0 +1,10 @@
+package ConferencePersistence;
+
+/**
+ * Created by Waiting on 23-May-17.
+ */
+public interface IRepository<ID, T> {
+    void save(T entity);
+    Iterable<T> findAll();
+    T findOne(ID id);
+}
