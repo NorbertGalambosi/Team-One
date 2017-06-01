b00c153f3fad7329fb0a87e0fe873a0fb2a5759e
NorbertGalambosi <norbert.galambosi@gmail.com>
2017-06-01 22:01:49 +0300

    Deleted
    Deleted

--- Conference/src/DomainClasses/Identifiable.java
+++
@@ -1,9 +0,0 @@
-package DomainClasses;
-
-/**
- * Created by Waiting on 20-May-17.
- */
-public interface Identifiable<ID> {
-    void setid(ID id);
-    ID getid();
-}
--- Conference/src/Validator/Validator_Exception.java
+++
@@ -1,7 +0,0 @@
-package Validator;
-
-public class Validator_Exception extends  Exception {
-    public Validator_Exception(String message){
-        super(message);
-    }
-}
